0ac7843f24fe48089b0903b383c4bd3eaf83014d
Dragos Tudorache <hedphug@gmail.com>
2024-12-30 04:50:02 +0200

    Initial Commit

---
+++ Dezka/DezkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DezkaApp.swift
+//  Dezka
+//
+//  Created by Dragos Tudorache on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct DezkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
