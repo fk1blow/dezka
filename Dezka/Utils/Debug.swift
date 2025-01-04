eb936dc9920cdbc3610f652d34498d4c79c9f52d
Dragos Tudorache <hedphug@gmail.com>
2025-01-04 21:15:20 +0200

    Create Debug.swift

---
+++ Dezka/Utils/Debug.swift
@@ -0,0 +1,16 @@
+struct Debug {
+  enum Level {
+    case info, warning, error
+  }
+
+  #if DEBUG
+    static let isEnabled = true
+  #else
+    static let isEnabled = false
+  #endif
+
+  static func log(_ message: String, level: Level = .info) {
+    guard isEnabled else { return }
+    print("[\(level)] \(message)")
+  }
+}
