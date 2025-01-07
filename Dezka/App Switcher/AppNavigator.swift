3a6d169874e51cfb930883cea62832a08f721fc2
Dragos Tudorache <hedphug@gmail.com>
2025-01-07 21:29:49 +0200

    Simplify AppNavigator state

--- Dezka/App Switcher/AppNavigator.swift
+++ Dezka/App Switcher/AppNavigator.swift
@@ -5,13 +5,17 @@
 
 import SwiftUI
 
+struct AppNavigatorState {
+  var appSearchQuery: String = ""
+  var visibleApps: [NSRunningApplication] = []
+  var navigationIndex: Int = 0
+}
+
 class AppNavigator: ObservableObject {
-  @Published private(set) var appListFilterQuery: String = ""
-  @Published private(set) var appsList: [NSRunningApplication] = []
-  @Published private(set) var navigationAtIndex: Int = 0
+  @Published private(set) var state = AppNavigatorState()
 
   init() {
-    appsList = getAppsWithWindows()
+    state = getInitialState()
 
     NSWorkspace.shared.notificationCenter.addObserver(
       forName: NSWorkspace.didLaunchApplicationNotification,
@@ -36,29 +40,24 @@ class AppNavigator: ObservableObject {
   }
 
   func navigateToNext() {
-    guard navigationAtIndex < appsList.count - 1 else { return }
-    navigationAtIndex += 1
+    guard state.navigationIndex < state.visibleApps.count - 1 else { return }
+    state.navigationIndex += 1
   }
 
   func navigateToPrevious() {
-    guard navigationAtIndex > 0 else { return }
-    navigationAtIndex -= 1
+    guard state.navigationIndex > 0 else { return }
+    state.navigationIndex -= 1
   }
 
   func resetNavigation() {
-    resetNavigationStart()
+    state.navigationIndex = 0
   }
 
   func activateSelectedApp() {
-    guard appsList.indices.contains(navigationAtIndex) else { return }
-    let targetApp = appsList[navigationAtIndex]
+    guard state.visibleApps.indices.contains(state.navigationIndex) else { return }
+    let targetApp = state.visibleApps[state.navigationIndex]
     targetApp.activate(options: [.activateIgnoringOtherApps])
-    // Debug.log("Activating app: \(targetApp.localizedName ?? "unknown")")
-    resetNavigationStart()
-  }
-
-  private func resetNavigationStart() {
-    navigationAtIndex = 0
+    resetNavigation()
   }
 
   private func handleApplicationLaunched(_ notification: Notification) {
@@ -66,7 +65,7 @@ class AppNavigator: ObservableObject {
       let launchedApp = userInfo[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication
     {
       guard launchedApp.bundleIdentifier != "ro.dragostudorache.Dezka" else { return }
-      appsList.append(launchedApp)
+      state.visibleApps.append(launchedApp)
     }
   }
 
@@ -75,7 +74,7 @@ class AppNavigator: ObservableObject {
       let terminatedApp = userInfo[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication
     {
       guard terminatedApp.bundleIdentifier != "ro.dragostudorache.Dezka" else { return }
-      appsList.removeAll { $0.processIdentifier == terminatedApp.processIdentifier }
+      state.visibleApps.removeAll { $0.processIdentifier == terminatedApp.processIdentifier }
     }
   }
 
@@ -85,27 +84,31 @@ class AppNavigator: ObservableObject {
     {
       guard activatedApp.bundleIdentifier != "ro.dragostudorache.Dezka" else { return }
 
-      var updatedAppList = appsList.filter {
+      var updatedAppList = state.visibleApps.filter {
         $0.processIdentifier != activatedApp.processIdentifier
       }
       updatedAppList.insert(activatedApp, at: 0)
-      appsList = updatedAppList
+      state.visibleApps = updatedAppList
     }
   }
 
-  private func getAppsWithWindows() -> [NSRunningApplication] {
+  private func getInitialState() -> AppNavigatorState {
     let runningApps = NSWorkspace.shared.runningApplications
 
-    return
+    let appsWithWindows =
       runningApps
       .filter { $0.bundleIdentifier != "ro.dragostudorache.Dezka" }
       .filter { app in
         guard !app.isHidden, app.activationPolicy == .regular else {
           return false  // Exclude hidden/system apps
         }
-        // This currently fails on runtime, so disregar it for now
-        // return hasWindows(runningApp: app)
         return true
       }
+
+    return AppNavigatorState(
+      appSearchQuery: "",
+      visibleApps: appsWithWindows,
+      navigationIndex: 0
+    )
   }
 }
