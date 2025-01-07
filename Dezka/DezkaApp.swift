45eabc686cc5186eab58ee43c2a696732886e0b3
Dragos Tudorache <hedphug@gmail.com>
2025-01-07 04:32:06 +0200

    Update DezkaApp.swift

--- Dezka/DezkaApp.swift
+++ Dezka/DezkaApp.swift
@@ -12,13 +12,13 @@ struct DezkaApp: App {
   @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
 
   var body: some Scene {
-//    WindowGroup {
-//      SettingsScreen()
-//    }
+    //    WindowGroup {
+    //      SettingsScreen()
+    //    }
     Settings {
       EmptyView()
-//      SettingsScreen()
-//        .frame(width: 400, height: 300)
+      //      SettingsScreen()
+      //        .frame(width: 400, height: 300)
     }
   }
 }
