39f83a8d6614f6462f4392e7556c89a0753e0b5d
Dragos Tudorache <hedphug@gmail.com>
2025-01-12 03:14:51 +0200

    Update AppSwitcherContentView.swift

--- Dezka/Main View/AppSwitcherContentView.swift
+++ Dezka/Main View/AppSwitcherContentView.swift
@@ -14,25 +14,25 @@ struct AppSwitcherContentView: View {
     VStack(
       alignment: .center, spacing: 0,
       content: {
-        ZStack(
-          alignment: .topLeading,
-          content: {
-            SearchInputView(text: $searchTerm)
-              .frame(height: 20)
-              .padding(.horizontal, 10)
-              .offset(x: 0, y: 6)
-              .overlay(VStack { Divider().background(Color(hex: "#383838")).offset(x: 0, y: 29) })
-              .frame(height: 45)
+        // ZStack(
+        //   alignment: .topLeading,
+        //   content: {
+        //     SearchInputView(text: $searchTerm)
+        //       .frame(height: 20)
+        //       .padding(.horizontal, 10)
+        //       .offset(x: 0, y: 6)
+        //       .overlay(VStack { Divider().background(Color(hex: "#383838")).offset(x: 0, y: 29) })
+        //       .frame(height: 45)
 
-            if searchTerm.isEmpty {
-              Text("Search for apps...")
-                .font(.system(size: 16, weight: .regular))
-                .foregroundStyle(Color(hex: "#767676"))
-                .offset(x: 15, y: 18)
-                .frame(height: 20)
-            }
-          }
-        )
+        //     if searchTerm.isEmpty {
+        //       Text("Search for apps...")
+        //         .font(.system(size: 16, weight: .regular))
+        //         .foregroundStyle(Color(hex: "#767676"))
+        //         .offset(x: 15, y: 18)
+        //         .frame(height: 20)
+        //     }
+        //   }
+        // )
 
         Spacer()
 
@@ -52,7 +52,7 @@ struct AppSwitcherContentView: View {
         ? EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
         : EdgeInsets(top: -30, leading: 0, bottom: 0, trailing: 0)
     )
-    .background(Color(hex: "#0D0D0D"))
+    .background(Color(hex: "#1E1E1E"))
     .onReceive(NotificationCenter.default.publisher(for: .applicationWillHide)) { _ in
       searchTerm = ""
     }
